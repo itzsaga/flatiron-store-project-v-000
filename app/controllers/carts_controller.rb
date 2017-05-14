061da82005db787e424a092b370c18142a41e8e4
Seth <itz.saga@gmail.com>
2017-05-14 17:50:01 -0500

    created show and checkout

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,18 @@
 class CartsController < ApplicationController
+  before_action :find_cart
 
+  def show
+  end
+
+  def checkout
+    @cart.checkout
+    current_user.current_cart = nil
+    redirect_to cart_path(@cart)
+  end
+
+  private
+
+  def find_cart
+    @cart = Cart.find(params[:id])
+  end
 end
