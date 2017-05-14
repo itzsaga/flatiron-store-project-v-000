7a678a69ad4ea32e337489420008743e4ea6d2c4
Seth <itz.saga@gmail.com>
2017-05-13 22:34:04 -0500

    cart specs passing

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,6 +4,16 @@ class Cart < ActiveRecord::Base
   has_many :items, through: :line_items
 
   def total
+    items.map(&:price).sum
+  end
 
+  def add_item(itemid)
+    item = line_items.find_by(item_id: itemid)
+    if line_items.include?(item)
+      item.quantity + 1
+      item
+    else
+      line_items.new(item_id: itemid)
+    end
   end
 end
